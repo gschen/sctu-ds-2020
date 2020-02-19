8d19c912d1698e85d8211b6c66bc57ecc3219023
llworldking <2812416175@qq.com>
2020-02-19 15:29:23 +0800

    666

--- 1906101101-梁林/test.py
+++ 1906101101-梁林/test.py
@@ -1,1 +1,1 @@
-print(666)                                                                                                       
+print(666)                                                          print(666)                                          
