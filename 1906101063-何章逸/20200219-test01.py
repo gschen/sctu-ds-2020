28efa2b38ff6d7b27b14d70ff354581810ea82f4
he2307563520 <2307563520@qq.com>
2020-02-19 16:53:03 +0800

    练习

---
+++ 1906101063-何章逸/20200219-test01.py
@@ -0,0 +1,52 @@
+str1='abcdef'
+print(str1[o])
+print(str1[0:4])
+a='abcd'
+'e' in a
+'v' not in a
+'a' in a
+
+
+
+print('我叫%s'%('张三'))
+print('我今年%d'%(10))
+
+list10=[1,2,3,44,5]
+len(list10)
+
+
+
+
+a='hi'
+print(a*3)
+
+
+
+
+
+for x in range(len(list1)):
+     print(x)
+
+list1=[1,2,3[4,5,6]]
+list1.append(7)
+list1.extend(1,2)
+print(list1)
+
+
+list1=[1,2,3[4,5,6]]
+list1.append(7)
+list1.extend(1,2)
+print(list1)
+
+
+list1=[4,7,9,0]
+list1.sort()
+list1.pop()
+print(list1)
+
+
+
+tup=('n',199,[6,6])
+print(tup)
+tup[1]=200
+
