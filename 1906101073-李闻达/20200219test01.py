19320b033b5305fdde911bbdf7732ab6b85f6c0b
Swain-Lee <1074717195@qq.com>
2020-02-19 17:04:32 +0800

    python复习

--- 1906101073-李闻达/20200219test01.py
+++ 1906101073-李闻达/20200219test01.py
@@ -24,23 +24,27 @@ print("我今年%d"%(10))
 #列表
 list=[1,2,3,[4,5,6]]
 print(len(list))
-for x in range(len(list)):
+
+#迭代
+for x in [1,2,3]:
     print(x)
 
 
-list=[5,4,2,8,3,8]
+#增加删除
 #list.append(7)
 #list.extend([1,2])
 #print(list)
 #print(list.index(2))
-list.pop()
-list.pop(1)
-#list=list.sort()
+#list.pop()
+#list.pop(1)
+#排序
+list=[5,4,2,8,3,8]
+list=list.sort()
 print(list)
 
 
 
 #元组
 tup=('s',100,[1,2])
-#tup[1]=200  报错
+#tup[1]=200  报错，元组中的元素不能更改
 print(tup)
