23186d0b56578b52d45fe31f450b927b2a2f60cd
L-lijiao <2771192897@qq.com>
2020-02-19 18:23:28 +0800

    01

---
+++ 1906101088-李娇/20200219text01.py
@@ -0,0 +1,34 @@
+a="hi"
+b="s"
+print(a+b)
+
+c='abcde '
+print(c*3)
+
+
+
+str1='abcdef'
+print(str1[1])
+print(str1[-5])
+print(str1[2:5])
+
+
+print("我是%s"%('李娇'))
+print('我今年%d岁啦！'%(5))
+
+
+list=[1,2,3,4,[a,b,c]]
+print(len(list))
+
+for x in [1,2,3,4]:
+    print(x)
+
+list1=[1,2,3,4,5,6]
+list1.append(7)
+list1.extend([3,4])
+#list1.pop(1)
+#list1.sort()
+print(list1)
+
+
+list2=[]
