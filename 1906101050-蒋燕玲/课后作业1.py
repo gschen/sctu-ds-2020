f97b9085301a6e56323574b79f49326fd0c37dc1
safe-andsoumd <2829611236@qq.com>
2020-02-20 17:03:21 +0800

    课后作业1

---
+++ 1906101050-蒋燕玲/课后作业1.py
@@ -0,0 +1,40 @@
+#1、求给定数的阶乘
+#def jiecheng(n):
+     #if n == 0 or n ==1:
+          #return 1
+     #else:
+         # return n * jiecheng(n-1)
+#print(jiecheng(6))
+
+
+
+#2、单利公式
+#def danli():
+     #p=input("请输入本金P：")
+     #t=input("请输入时间T:")
+     #r=input("请输入利率r:")
+     #c=float((float(p)*float(t)*float(r))/100)
+     #print("单利是: %d" %(c))
+#danli()
+
+
+
+#3、查找数组中的最大元素
+#list=[14,25,98,75,23,1,4,56,59]
+#list.sort()
+#print(list[-1])
+
+
+#4、求数组前n个数的平方和
+lis1=[14,25,98,75,23,1,4,56,59]
+s=0
+n=int(input("计算前n个数,n为："))
+m=list(lis1[:n])
+if n <9:
+     for x in m:
+          x=x**2
+          s+=x
+     print("平方和为: %d" %(s))
+
+else:
+     print("抱歉，n为正整数且不能大于数组长度9,请重新输入。")
