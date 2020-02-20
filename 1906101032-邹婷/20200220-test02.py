4248028502a2e0431262c624134bd7a613c270ea
0805wg <2856875640@qq.com>
2020-02-20 23:32:29 +0800

    课后作业1

--- 1906101032-邹婷/20200220-test02.py
+++ 1906101032-邹婷/20200220-test02.py
@@ -19,12 +19,13 @@ print(list[-1])
 
 #第四题
 list1=[14,25,98,75,23,1,4,56,59]
-x=int(input())
-if 1<=x<=9:
-    sum=0
-    for i in list1[:x]:
-        sum=sum+i*i
-    print(sum)
+x=int(input('x='))
+list1=list1[:x]
+sum=0
+for i in list1:
+    if x>0:
+        sum += i*i       
+print(sum)
 
 #第五题
 list2=[14,25,98,75,23,1,4,56,59]
