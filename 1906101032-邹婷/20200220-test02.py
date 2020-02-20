b5c29f0caa5cb67c02d7f59372b1907376cab3a4
0805wg <2856875640@qq.com>
2020-02-20 22:42:26 +0800

    课后作业1

---
+++ 1906101032-邹婷/20200220-test02.py
@@ -0,0 +1,34 @@
+#第一题
+def factorial(n):
+    result=n 
+    for i in range (1,n):
+        result *= i 
+    return result
+print (factorial(18))
+
+#第二题
+def addMulti(P,T,R):
+    return (P*T*R)/100
+print(addMulti(20000,10,10))
+
+#第三题
+from random import*
+list=[14,25,98,75,23,1,4,56,59]
+list.sort()
+print(list)
+print(list[-1])
+
+#第四题
+list1=[14,25,98,75,23,1,4,56,59]
+x=int(input())
+if 1<=x<=9:
+    sum=0
+    for i in list1[:x]:
+        sum=sum+i*i
+    print(sum)
+
+#第五题
+list2=[14,25,98,75,23,1,4,56,59]
+a,b=map(int,input().split())
+list2[a],list2[b]=list2[b],list2[a]
+print(list2)
