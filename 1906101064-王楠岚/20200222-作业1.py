49ab3f899308a5706e3e0f46f119dccc0987dd26
wangnanlan <1146964788@qq.com>
2020-02-22 21:09:17 +0800

    第一次作业

--- 1906101064-王楠岚/20200222-作业1.py
+++ 1906101064-王楠岚/20200222-作业1.py
@@ -30,10 +30,11 @@ def nian(m):
 print(sum(map(nian,list[:n])))
 
 #第五题
-a,b = map(int,input().split())
-nums = [14,25,98,75,23,1,4,56,59]
-num_a,num_b = nums[a],nums[b]
-nums[b],nums[a] = num_a,num_b
+a,b=map(int,input().split())
+nums=[14,25,98,75,23,1,4,56,59]
+num_a,num_b=nums[a],nums[b]
+nums[b],nums[a]=num_a,num_b
 print(nums)
+
 
 
