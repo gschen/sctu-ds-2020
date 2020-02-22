f53a2dc0b7b718057f189cfddf41084da1ac9380
dengjing0207 <2807097015@qq.com>
2020-02-22 17:52:18 +0800

    作业

---
+++ 1906101026邓静/20200123.py/1,2.py
@@ -0,0 +1,22 @@
+#求给定数的阶乘
+def H(n):
+    error_num = [1,10,20,30,40,50]
+    if n == 0 or n == 1:
+        return 1
+    if n in error_num:
+        return "错误"
+    else:
+        return n*H(n-1)
+a=H(5)
+print(a)
+
+#单利公式
+import math
+P=int(input("p=:"))
+R=int(input("G=:"))
+T=int(input("T=:"))
+Q=(P*R*T)/100
+print(Q)
+
+
+
