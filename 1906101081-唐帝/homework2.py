aa216ce4d6b7e04fee8e7c4defa51d9a0d648318
476072478 <476072478@qq.com>
2020-02-22 15:37:59 +0800

    Enter

---
+++ 1906101081-唐帝/homework2.py
@@ -0,0 +1,6 @@
+#使用单利公式。
+
+
+a, b, c = map(int, input().split())
+m=a*b*c
+print(m)
---
+++ 1906101081-唐帝/homework5.py
@@ -0,0 +1,13 @@
+#交换列表中的任意两个元素。
+
+
+
+
+
+list=[14,25,98,75,23,1,4,56,59]
+a,b=map(int,input('请输入需要交换数字的位置,用逗号隔开：').split(','))
+n=list[a]
+m=list[b]
+list[a]=m
+list[b]=n
+print(list)
