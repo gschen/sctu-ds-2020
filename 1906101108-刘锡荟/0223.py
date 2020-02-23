0659e2826cc9f6f87c95c92aa5bdb1d8ec685da1
liuxihui15 <2806940759@qq.com>
2020-02-23 14:37:20 +0800

    课堂练习

---
+++ 1906101108-刘锡荟/0223.py
@@ -0,0 +1,29 @@
+a='hi'
+b='s'
+print(a+b)
+#重复字符
+c='hi'
+print(c*3)
+str1='abcde'
+print(str1[0])
+print(str[-1])
+print(str1[0:4])
+#格式化输出
+print('我叫%s'%('张三'))
+print('我今年%d'%(10))
+#列表
+lis1=[1,2,3,[4,5,6,]]
+print(len(lis1))
+for x in range(len(lis1)):
+    print(lis1[x])
+lis2=[5,4,2,8,3,8]   
+lis2.append(7)
+lis2.extend([1,2])
+lis2.pop(1)
+print(lis2) 
+lis1.sort()
+print(lis1)
+#元组
+tup('s',100,[1,2])
+tup[1]=200
+print(tup)
