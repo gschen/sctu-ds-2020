4e5a7f8d71e070adf7f4ab9d2cb537fb3f901c99
lizhaohai <2583342066@qq.com>
2020-02-25 16:51:45 +0800

    笔记

---
+++ 1906101021-李肇海/Day0225/test03.py
@@ -0,0 +1,17 @@
+# sum = 0
+# a = 1
+# # while(a <= 100):
+# #     sum = sum + a
+# #     a = a+1
+# # print(sum)
+
+# while(a == 1):
+#     print('无限循环中！！')
+
+
+count = 0
+while count < 5:
+    ptint(str(count),'<5')
+    count = count + 1
+else:
+    print(str(count)+'=5')
