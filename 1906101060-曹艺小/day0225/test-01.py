32570ced4bcbdde0d1bb39520dd55c28a69db10b
曹艺小 <1781029805@qq.com>
2020-02-25 17:30:40 +0800

    20200225test

---
+++ 1906101060-曹艺小/day0225/test-01.py
@@ -0,0 +1,16 @@
+# age = int(input("请输入你的年龄"))
+# if age >= 18:
+#     print("你已经成年啦")
+# else:
+#     print("你是未成年")
+
+
+num1 = 25
+num2 = 1
+while(num1 != num2):
+    num2 = int(input("请输入你的数字："))
+    if num1 > num2:
+        print("你输入的值小了")
+    elif num1 <num2:
+        print("你输入的值大了")
+print("猜对了")
