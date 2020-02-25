d8a8d3cc998829e4e03e1d4ab65cf73c207e1915
huangfuqin29 <2374104001@qq.com>
2020-02-25 17:59:53 +0800

    课上练习

---
+++ 1906101030-黄富琴/Day0225/test3.py
@@ -0,0 +1,16 @@
+#a=1
+#while(a<=100):
+#    sum=sum+a
+#    a=a+1
+#print(sum)
+
+#while():
+#    print('无线循环中！！！')
+
+count = 0
+
+while count < 5:
+    print(count)
+    count=count + 1
+else:
+    print(count+'=5')
