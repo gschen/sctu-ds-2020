8fab2f926c52507be7c9b4b53e82c987470f9a18
luoyunxi7 <2205382545@qq.com>
2020-02-28 19:43:12 +0800

    20200226课后作业

---
+++ 1906101004-唐娜/day0226/作业4.py
@@ -0,0 +1,7 @@
+#4.	（使用循环和判断）有四个数字：1、2、3、4，能组成多少个互不相同且无重复数字的三位数？各是多少？
+
+for i in range(1,5):
+    for j in range(1,5):
+        for k in range(1,5):
+            if(i!=k) and (i!=j) and (j!=k):
+                print(i,j,k)
