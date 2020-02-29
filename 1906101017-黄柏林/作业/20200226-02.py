b5ed37f98ece9feb5af693c0c74ea2a246a89932
黄柏林 <1505667570@qq.com>
2020-02-29 22:09:34 +0800

    26日作业

---
+++ 1906101017-黄柏林/作业/20200226-02.py
@@ -0,0 +1,16 @@
+# 2.	（使用def函数完成）编写一个函数，要求输入成绩，输出成绩的等级。90-100为A，80-90为B，60-80为C，60分以下为D.
+# 样例输入
+# 85
+# 样例输出
+# B
+
+def cj(x):
+    if x>=90:
+        return 'A'
+    elif x>=80 and x<90:
+        return 'B'
+    elif x>=60 and x<80:
+        return 'C'
+    elif x<60:
+        return 'D'
+print(cj(65))
