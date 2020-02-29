4c50797b40a86f30cb1a7a9ad33926387a43d0fd
ASUS <896301367@qq.com>
2020-02-29 22:26:38 +0800

    2020.02.24课堂讲解

---
+++ 1906101071-胡载阳/20200224/test-04.py
@@ -0,0 +1,20 @@
+# list1 = [1,2,3,4,5]
+# for i in list1:
+#     print(i)
+
+# str1 = 'abcdefg'
+# for j in str1:
+#     print(j)
+
+# 
+
+
+
+
+
+
+
+for i in range(0,11,3)
+
+
+print(list(range(5)))
