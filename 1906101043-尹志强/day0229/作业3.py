f3a72052085f57e87f0bd3d6078e1ff0daa3ce77
19363 <1936373170@qq.com>
2020-02-29 12:13:30 +0800

    修改

--- 1906101043-尹志强/day0229/作业3.py
+++ 1906101043-尹志强/day0229/作业3.py
@@ -1,17 +1,9 @@
 def list3(x):
-    if type(x)==list:
-        for i in x:
-            if int(i)%2==0:
-                x.remove(i)
-            else:
-                a=x
-        return a
-    if type(x)==tuple:
         list1=[]
         for i in x:
             if int(i)%2!=0:
                 list1.append(i)
         return list1
 
-print(list3((1,2,3,4,5,6,7)))
+print(list3([1,2,3,4,5,6,7]))
             
