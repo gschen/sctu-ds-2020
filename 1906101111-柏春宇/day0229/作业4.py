313cd16e88a131ee475168842529cb2a8658d93b
柏春宇 <2785504109qq.com>
2020-03-01 13:27:21 +0800

    ZY

---
+++ 1906101111-柏春宇/day0229/作业4.py
@@ -0,0 +1,7 @@
+a_set=[1,2,3,4]
+for i in a_set:
+    for j in a_set:
+        for k in a_set:
+            if i != j and i!=k and j!=k:
+                print(i,j,k,end='')
+                print()
