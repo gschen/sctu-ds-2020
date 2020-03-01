daf839ac8a5d85a6d4e9a51cd9f937151fadb028
lihelong1 <857788573@qq.com>
2020-03-01 17:20:22 +0800

    20200219课后作业

---
+++ 1906101007-李和龙/Day0225/3.py
@@ -0,0 +1,17 @@
+"""
+3.	（使用def函数完成）找出传入函数的列表或元组的奇数位对应的元素，并返回一个新的列表
+
+样例输入
+	1,2,3,4,5,6,7
+样例输出
+1, 3, 5, 7
+
+"""
+
+def new_list(lis):
+    list1 = []
+    for i in range(len(lis)):
+        if i % 2 == 0:
+            list1.append(lis[i])
+    return list1
+print(new_list([1,2,3,4,5,6,7,1,2]))
---
+++ 1906101007-李和龙/Day0225/5.py
@@ -0,0 +1,11 @@
+"""
+使用循环和判断）输入三个整数x,y,z，请把这三个数由小到大输出。
+
+"""
+a,b,c = map(int,input().split())
+list1 = [a,b,c]
+for i in range(3):
+    for m in range(i,3):
+        if list1[i] > list1[m]:
+            list1[i],list1[m] = list1[m],list1[i]
+print(list1)
