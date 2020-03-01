5474aef14793e124ec3cdd41f8c973c308446adf
wz-fh <1452308054@qq.com>
2020-03-01 17:59:54 +0800

    课后作业

--- 1906101008-韦平香/day0226/作业1.py
+++ 1906101008-韦平香/day0226/作业1.py
@@ -53,11 +53,11 @@
 #7.	（使用def函数完成）写函数，统计字符串中有几个字母，几个数字，几个空格，几个其他字符，并返回结果
 #n=input('请输入：')
 #for i in list:
-    if i.isdigit():
-        int_count += 1
-    elif i.isalnum():
-        str_count += 1
-else:
-    other_count += 1
-print('数字=%d,字母=%d,其它=%d'%(int_count,str_count,other_count))
+#   if i.isdigit():
+#        int_count += 1
+#    elif i.isalnum():
+#        str_count += 1
+#else:
+#    other_count += 1
+#print('数字=%d,字母=%d,其它=%d'%(int_count,str_count,other_count))
 
