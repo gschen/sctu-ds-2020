38efdad2a4267db5bc23de43870db24739ae2940
董照岚 <3310757070@qq.com>
2020-03-03 15:01:43 +0800

    改

--- 1906101092董照岚/day0225/作业4.py
+++ 1906101092董照岚/day0225/作业4.py
@@ -5,7 +5,7 @@ for i in range(1,5):
     for j in range(1,5):
         for k in range(1,5):
             if i !=j and i !=k and j!=k:
-                print("{}{}{}".format(i,j,k),end=" ")
+                print("{}{}{}".format(i,j,k),end=" ")#相当于占位符，一个大括号代表一个占位符
                 x +=1
 print("")
 print("共{}种组合".format(x))
