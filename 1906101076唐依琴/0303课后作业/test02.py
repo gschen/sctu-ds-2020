c559a2d1d5f59974f06140eb21fac67b7c823a3c
Tangyiqin1211 <2985823977@qq.com>
2020-03-06 19:34:39 +0800

    0303课后作业

---
+++ 1906101076唐依琴/0303课后作业/test02.py
@@ -0,0 +1,21 @@
+class Person(object):
+    def __init__(self,name,age,sex):
+        self.name=name
+        self.age=age
+        self.sex=sex
+    def personInfo(self):
+        print(("name:%s,age:%s,sex:%s")%(self.name,self.age,self.sex))
+class Teacher(Person):
+    def __init__(self,name,age,sex,college,professional):
+        Person.__init__(self,name,age,sex)
+        self.college=college
+        self.professional=professional
+    def personInfo(self):
+        super().personInfo()
+        print('college',self.college)
+        print('professional',self.professional)
+    def teachObj(self):
+        print('今天讲了：面向对象设计程序')
+yi=Teacher('Lee',27,'男','信息与工程学院','信息管理与信息系统')
+yi.personInfo()
+yi.teachObj()
