7e7d65da9e6396ef5210c940070fa11138ff15e6
wangxi2 <2359362331@qq.com>
2020-03-08 19:47:30 +0800

    zuoye

---
+++ 1906101059-王曦/day20200308/作业.py
@@ -0,0 +1,26 @@
+class Person(object):
+    def __init__(self,name,age,sex):
+        self.name = name
+        self.age = age
+        self.sex = sex
+
+    def personinfo(self):
+        print(self.name)
+        print(self.age)
+        print(self.sex)
+
+class Teacher(Person):
+    def __init__(self,collage,professional):
+        self.collage = collage
+        self.professional = professional
+
+    def personinfo(self):
+        print(self.collage)
+        print(self.professional)
+
+    def teachojb(self):
+        return ('今天讲了：面向对象设计程序')
+
+ def study(self,teacher):
+        print('我是%s,老师%s,我终于学会了！')
+
