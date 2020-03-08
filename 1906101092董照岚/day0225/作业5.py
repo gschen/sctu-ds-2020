d8edab1b9f972216061c19b86089f760486f21c8
董照岚 <3310757070@qq.com>
2020-03-08 10:39:49 +0800

    改

--- 1906101092董照岚/day0225/作业5.py
+++ 1906101092董照岚/day0225/作业5.py
@@ -2,13 +2,22 @@
 x=int(input("输入一个整数"))
 y=int(input("输入一个整数"))
 z=int(input("输入一个整数"))
-if x > y:
-    x,y=y,x
-if x > z:
-    x,z=x,z
-if y > z:
-    y,z=z,y    
-print(x,y,z)
+if x >= y and x >= z:
+    if y >= z:
+        print('xyz')
+    else:
+        print('xzy')
+elif y >= z and y >= x:
+    if z >= x:
+        print('yzx')
+    else:
+        print("yxz")
+elif z >= x and z >=y:
+    if x >= y:
+        print('zxy')
+    else:
+        print('zyx')
+    
 
 
 #方法二
