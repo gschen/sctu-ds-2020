91d9e62e98430f6040d13d984002067aa3e449b7
wanghongsu <3162938599@qq.com>
2020-03-14 20:29:00 +0800

    0310课堂及作业

---
+++ 1906101074-王泓苏/Day0310/test01.py
@@ -0,0 +1,15 @@
+class Student():
+    def __init__(self,name,age,course):
+        self.name=name
+        self.age=age
+        self.course=course
+    def get_name(self):
+        print('姓名:',self.name)
+    def get_age(self):
+        print('年龄:',self.age) 
+    def get_course(self):
+        print('最高分:',max(self.course))
+a=Student('Tom',19,[90,98,88])
+a.get_name()
+a.get_age()
+a.get_course()    
