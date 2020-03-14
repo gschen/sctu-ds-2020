c34105a5ff6d4a7665edb0d35e21964b5c4c7452
st13739438729 <1837211345@qq.com>
2020-03-15 00:51:57 +0800

    这道题的豪华升级版

--- 1906101105-石涛/day0310/课后作业/作业1.py
+++ 1906101105-石涛/day0310/课后作业/作业1.py
@@ -37,7 +37,7 @@ class Game():
     def palyGame(self):
         n=0
         print('你被来历不明的敌人追杀！！！\n却偶然跌落到了一片神秘空间.\n这里充满了各种不同颜色的火焰.\n')
-        print('这里的一位神秘强者说与你有缘，便让你吞噬了这里的所有火焰.\n并且学会了这位强者的所有技能\n')
+        print('这里的一位神秘强者说与你有缘，便让你吞噬了这里的所有火焰.\n并且学会了这位强者的所有技能....\n')
         print('此时你已经很强大了！！！\n现在你回到原来的世界...')
         print('-'*30)
         print('不巧，神秘人却找到了你！！！\n此时的你们的数据是：\n')
@@ -53,18 +53,26 @@ class Game():
             b_out=self.b.play_B()
             n+=1
             print('第{}回合'.format(n))
-            if self.b.Evade>=random.random():
-                print('{}使出了一招{}，但{}身形一闪，躲避了{}！！！'.format(self.a.name,a_out[1],self.b.name,a_out[1]))
-            else:
-                if self.b.Critical_hit_rate>=random.random():
-                    print('{}使出了一招{}，击中了{}的要害，造成{}点伤害！！！'.format(self.a.name,a_out[1],self.b.name,(a_out[0]+1)*self.a.Aggressivity*2))
-                    self.b.Blood_volume-=(a_out[0]+1)*self.a.Aggressivity*2
+            m=input('你是进攻还是防守？(请输入1-->(进攻)或2-->(防守))')
+            if m=='1':
+                if self.b.Evade>=random.random():
+                    print('{}使出了一招{}，但{}身形一闪，躲避了{}！！！'.format(self.a.name,a_out[1],self.b.name,a_out[1]))
                 else:
-                    print('{}使出了一招{}，对{}造成了{}点伤害！'.format(self.a.name,a_out[1],self.b.name,(a_out[0]+1)*self.a.Aggressivity))
-                    self.b.Blood_volume-=(a_out[0]+1)*self.a.Aggressivity
+                    if self.b.Critical_hit_rate>=random.random():
+                        print('{}使出了一招{}，击中了{}的要害，造成{}点伤害！！！'.format(self.a.name,a_out[1],self.b.name,(a_out[0]+1)*self.a.Aggressivity*2))
+                        self.b.Blood_volume-=(a_out[0]+1)*self.a.Aggressivity*2
+                    else:
+                        print('{}使出了一招{}，对{}造成了{}点伤害！'.format(self.a.name,a_out[1],self.b.name,(a_out[0]+1)*self.a.Aggressivity))
+                        self.b.Blood_volume-=(a_out[0]+1)*self.a.Aggressivity
+            if m=='2':
+                self.a.Evade+=0.4
 
             if self.a.Evade>=random.random():
-                print('{}使出了一招{}，但{}身形一闪，躲避了{}！！！'.format(self.b.name,b_out[1],self.a.name,b_out[1]))
+                if m=='2':
+                    print('{}使出了一招{}，但{}身形一闪，躲避了{}，并且回身一击对{}造成800点伤害！！！'.format(self.b.name,b_out[1],self.a.name,b_out[1],self.b.name))
+                    self.b.Blood_volume-=200
+                else:
+                    print('{}使出了一招{}，但{}身形一闪，躲避了{}！！！'.format(self.b.name,b_out[1],self.a.name,b_out[1]))
             else:
                 if self.a.Critical_hit_rate>=random.random():
                     print('{}使出了一招{}，击中了{}的要害，造成{}点伤害！！！'.format(self.b.name,b_out[1],self.a.name,(b_out[0]+1)*self.a.Aggressivity))
@@ -72,6 +80,8 @@ class Game():
                 else:
                     print('{}使出了一招{}，对{}造成了{}点伤害！'.format(self.b.name,b_out[1],self.a.name,((b_out[0]+1)*self.b.Aggressivity)/2))
                     self.a.Blood_volume-=((b_out[0]+1)*self.b.Aggressivity)/2
+            if m=='2':
+                self.a.Evade-=0.4
 
             if self.b.Blood_volume<=0:
                 print('-'*60)
