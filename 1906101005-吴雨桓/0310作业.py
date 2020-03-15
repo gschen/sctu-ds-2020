36a463c6f8d0afa70d611c4f8bae5d420710351b
wuyuhuan7 <1280485671@qq.com>
2020-03-15 19:57:33 +0800

    zuoye

--- 1906101005-吴雨桓/0310作业.py
+++ 1906101005-吴雨桓/0310作业.py
@@ -8,7 +8,6 @@ class A():
         self.crit=crit
         self.dodge=dodge
 
-
     def attack_B(self,B):
         if random.random() <= self.dodge:
             print('孙悟空攻击被闪避')
---
+++ 1906101005-吴雨桓/课堂/0310课堂.py
@@ -0,0 +1,52 @@
+# class Student():
+#     def __init__(self,name,age,score):
+#         self.name=name
+#         self.age=age
+#         self.score=score
+#     def get_name(self):
+#         print(self.name)
+#     def get_age(self):
+#         print(self.age)
+#     def get_course(self):
+#         print(max(self.score))
+# m=Student("x",20,[98,95,100])
+# m.get_name()
+# m.get_age()
+# m.get_course()
+#
+import random
+class Person():
+    def __init__(self,name):
+        self.name=name
+        self.score=0
+    def fingerPlay(self):
+        game=["布","剪刀","石头"]
+        index=random.randint(0,2)
+        #random.choice(geme)
+        return game[index]
+class Game():
+    def __init__(self,number,aname,bname):
+        self.number=number
+        self.a=Person(aname)
+        self.b=Person(bname)
+    def playGame(self):
+        for i in range(self.number):
+            a_out=self.a.fingerPlay()
+            b_out=self.b.fingerPlay()
+            print()
+            if a_out==b_out:
+                print("双方平局，出的是%s" % (a_out))
+            elif (a_out=="石头" and b_out=="剪刀") or (a_out=="布" and b_out=="石头") or (a_out=="剪刀" and b_out=="布"):
+                print("%s获得胜利,出的是%s,%s出的是%s" % (self.a.name,a_out,self.b.name,b_out))
+                self.a.score+=1
+            else:
+                self.b.score+=1
+                print("%s获得胜利,出的是%s,%s出的是%s" % (self.b.name,b_out,self.a.name,a_out))
+        if self.a.score>self.b.score:
+            print("恭喜%s获得游戏胜利,得分%d" % (self.a.name,self.a.score))
+        elif self.a.score<self.b.score:
+            print("恭喜%s获得游戏胜利,得分%d" % (self.b.name,self.b.score))
+        else:
+            print("双方平局，不分胜负")
+one=Game(600,"up","down")
+one.playGame()
