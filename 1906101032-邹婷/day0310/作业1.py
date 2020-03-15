b7fff55d36902d5ddb8989d85f1444974cacd0f1
0805wg <2856875640@qq.com>
2020-03-15 18:10:05 +0800

    作业

--- 1906101032-邹婷/day0310/作业1.py
+++ 1906101032-邹婷/day0310/作业1.py
@@ -1,52 +1,57 @@
 import random
 class Person():
-    def __init__(self,name,xueliang,gongjili,baojilv,shanbilvu):
+    def __init__(self,name,xueliang,gongjili,baojilv,shanbilv):
         self.name=name
         self.xueliang=xueliang
         self.gongjili=gongjili
         self.baojilv=baojilv
-        self.shanbilvu=shanbilvu
+        self.shanbilv=shanbilv
     def role(self):
-        print('玩家名称:%s,血量:%s,攻击力:%s,暴击率:%s,闪避率:%s'%(self.name,self.xueliang,self.gongjili,self.baojilv,self.shanbilv))
+        print('玩家名称:%s\n血量:%s\n攻击力:%s\n暴击率:%s\n闪避率:%s'%(self.name,self.xueliang,self.gongjili,self.baojilv,self.shanbilv))
+a=Person('A家',500,500,0.2,0.4)
+b=Person('B家',600,600,0.3,0.6)
+a.role()
+b.role()
 class Game():
     def __init__(self):
-        self.aname=a
-        self.bname=b
+        self.person1=a
+        self.person2=b
     def playGame(self):
+        print('游戏开始')
         count=0
-        while self.aname.xueliang>0 and self.bname.xueliang>0:
+        while self.person1.xueliang>0 and self.person2.xueliang>0:
             count+=1
-            if random.random()<=self.aname.baojilv:
-                if random.random()<=self.bname.shanbilv:
-                    self.bname.xueliang-=0
+            if random.random()<=self.person1.baojilv:
+                if random.random()<=self.person2.shanbilv:
+                    self.person2.xueliang-=0
                 else:
-                    self.bname.xueliang-=self.aname.gongjili*2
+                    self.person2.xueliang-=self.person1.gongjili*2
             else:
-                if random.random()<=self.bname.shanbilv:
-                    self.bname.xueliang-=0
+                if random.random()<=self.person2.shanbilv:
+                    self.person2.xueliang-=0
                 else:
-                    self.bname.xueliang-=self.aname.gongjili
-            if random.random()<=self.bname.baojilv:
-                if random.random()<=self.aname.shanbilv:
-                    self.aname.xueliang-=0
+                    self.person2.xueliang-=self.person1.gongjili
+            if random.random()<=self.person2.baojilv:
+                if random.random()<=self.person1.shanbilv:
+                    self.person1.xueliang-=0
                 else:
-                    self.aname.xueliang-=self.bname.gongjili*2
+                    self.person1.xueliang-=self.person2.gongjili*2
             else:
-                if random.random()<=self.aname.shanbilv:
-                    self.bname.xueliang-=0
+                if random.random()<=self.person1.shanbilv:
+                    self.person2.xueliang-=0
                 else:
-                    self.aname.xueliang-=self.bname.gongjili
-            if self.aname.xueliang<=0:
-                self.aname.xueliang=0
-            if self.bname.xueliang<=0:
-                self.bname.xueliang=0
+                    self.person1.xueliang-=self.person2.gongjili
+            if self.person1.xueliang<=0:
+                self.person1.xueliang=0
+            if self.person2.xueliang<=0:
+                self.person2.xueliang=0
             print('第%d攻击:'%count)
-            print('A家:剩余血量:%d'%self.aname.xueliang)
-            print('B家:剩余血量:%d'%self.bname.xueliang)
-        if self.aname.xueliang==0:
-            print('经过%d轮攻击,A家胜利,'%count)
+            print('A家:剩余血量:%d'%self.person1.xueliang)
+            print('B家:剩余血量:%d'%self.person2.xueliang)
+        if self.person1.xueliang==0:
+            print('经过%d轮攻击,A家胜利.'%count)
         else:
-            print('经过%d轮攻击,B家胜利,'%count)
+            print('经过%d轮攻击,B家胜利.'%count)
 a=Person('A家',500,500,0.2,0.4)
 b=Person('B家',600,600,0.3,0.6)
 c=Game()
