bdd21527b7d230ddcd445d7a03be4ed4748f08ce
llworldking <2812416175@qq.com>
2020-03-28 10:11:49 +0800

    k

--- 1906101101-梁林/day0324/课堂练习/test01.py
+++ 1906101101-梁林/day0324/课堂练习/test01.py
@@ -5,6 +5,12 @@ class Node:
 class List:
     def __init__(self):
         self.head=Node(-1)
+
+
+
+
+
+    #头插
     def insert_before(self,date):
         for i in date:
             node=Node(i)
@@ -15,13 +21,48 @@ class List:
                 self.head.next=node
 
 
+
+
+
+    #尾插
     def insert__tail(self,date):
         tail=self.heaad.next
         for i in date:
             node=Node(i)
             if tail is Node:
                 self.head.next=node
-                tail=node
+                tail=node#记录每一次最后元素，方便下一次链接
             else:
                 tail.next=node
                 tail=node
+
+
+
+
+    #删除重复元素
+    def clear_repetition(self):
+        cur=self.head
+        while cur:
+            while cur.next and cur.value==cur.next.value:#重复进下一个节点
+                cur.next=cur.next.next
+            cur=cur.next
+
+
+
+            
+
+    #在i前插入新节点
+    def list_element_add(self,i,value):
+        node_new=Node(value)#新建新节点
+        index=0
+        node=self.head.next
+        while node:
+            index+=1
+            if index==i-1:#找到i-1节点
+                break
+            node=node.next
+        if node is Node:
+            return False
+        
+        node_new.next=node.next#新建链接
+        node.next=node_new
