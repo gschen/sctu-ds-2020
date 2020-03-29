7a19d7ca445514680b8df046fa6e847abca1c277
Tangyiqin1211 <2985823977@qq.com>
2020-03-29 17:19:41 +0800

    0324zuoye

---
+++ 1906101076唐依琴/0324作业/作业.py
@@ -0,0 +1,23 @@
+class ListNode:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class Solution:
+    def middleNode(self, head):
+        cur=head
+        count=0
+        while cur!=None:
+            count+=1
+            cur=cur.next
+        if count % 2==0:
+            a=count/2+1
+        else:
+            a=(count+1)/2
+        cur=head
+        a=a-1
+        while a:
+            cur=cur.next
+            a=a-1
+        return cur
