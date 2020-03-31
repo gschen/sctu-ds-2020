619afc9598e0a86bed3d347cb58667b356541dfc
LEGION <969970279@qq.com>
2020-03-31 16:24:40 +0800

    biji

--- 1906101045蓝昊杰/课堂记录/day0331-蓝昊杰.py
+++ 1906101045蓝昊杰/课堂记录/day0331-蓝昊杰.py
@@ -34,3 +34,55 @@ print(stack.is_empty())
 print(stack.top())
 print(stack.pop())
 print(stack.top())
+
+
+
+
+
+
+
+class Node(object):
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+class Stack(object):
+    def __init__(self):
+        self.node=Node(Node)
+        self.head=self.node
+        self.size=0
+    def is_empty(self):
+        return self.size==0
+    def get_size(self):
+        return self.size
+    def push(self,data):
+        noade=Node(data)
+        node.next=self.head.next
+        self.head.next=node
+        self.size+=1
+    def pop(self):
+        if not self.is_empty():
+            current_node=self.head.next
+            if self.get_size()==1:
+                self.head.next=Node
+                self.size-=1
+            else:
+                self.head.next=self.head.next.next
+                self.size-=1
+                return current_node.data
+        else:
+            print("栈为空")
+    def top(self):
+        if not self.is_empty():
+            return self.head.next.data
+        else:
+            print("栈为空")
+
+
+
+s=Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+print (s.get_size())
+print(s.is_empty())
+print(s.top())
