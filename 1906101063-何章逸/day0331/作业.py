ede8e23c3804e7c6ab0166f3df9b47a2d5d13917
he2307563520 <2307563520@qq.com>
2020-04-05 19:04:01 +0800

    作业

--- 1906101063-何章逸/day0331/作业.py
+++ 1906101063-何章逸/day0331/作业.py
@@ -1,36 +1,35 @@
-class Stack():
-    def __init__(self,limit = 10):#初始化一个栈
-        self.stack = []
-        self.limit = limit
+# 你现在是网球比赛记录员。
+# 给定一个字符串列表，每个字符串可以是以下四种类型之一：
+# 1.整数：您在本轮中获得的积分数。
+# 2. "+"：表示本轮获得的得分是前两轮有效 回合得分的总和。
+# 3. "D"：表示本轮获得的得分是前一轮有效 回合得分的两倍。
+# 4. "C"：表示您获得的最后一个有效 回合的分数是无效的，应该被移除。
+# 每一轮的操作都是永久性的，可能会对前一轮和后一轮产生影响。
+# 你需要返回你在所有回合中得分的总和。
+# 示例 1:
+# 输入: ["5","2","C","D","+"]
+# 输出: 30
+# 解释: 
+# 第1轮：你可以得到5分。目前总分是：5。
+# 第2轮：你可以得到2分。目前总分是：7。
+# 第3轮：第2轮的数据无效。目前总分是：5。
+# 第4轮：你可以得到10分（第2轮的数据已被删除）。总数是：15。
+# 第5轮：你可以得到5 + 10 = 15分。总数是：30。
 
-    def is_empty(self):#判断是否为空
-        return len(self.stack)==0
 
-    def push(self,data):#入栈
-        if len(self.stack)>=self.limit:
-            print('栈溢出')
-        else:
-            self.stack.append(data)
-
-    def pop(self):#弹出栈顶元素
-        if self.stack:
-            return self.stack.pop()
-        else:
-            print('空战不能弹出')
-    
-    def top(self):#查找栈顶元素
-        if self.stack:
-            return self.stack[-1]
-
-    def size(self):#长度
-        return len(self.stack)
-
-stack = Stack()
-print(stack.size())
-
-stack.push(1)
-stack.push(2)
-stack.push(3)
-stack.push(4)
-print(stack.size())
-print(stack.is_empty())
+class Some():
+    def pig(self,gay):
+        result=[]
+        for i in gay:
+            s=len(result)
+            if i=="+":
+                result.append(result[s-1]+result[s-2])
+            elif i=="C":
+                result.pop()
+            elif i=="D":
+                result.append(result[s-1]*2)
+            else:
+                result.append(int(i))
+        return sum(result)
+a=Some()
+print(a.pig(["5","2","C","D","+"]))    
