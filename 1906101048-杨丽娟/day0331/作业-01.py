d8cf8c8983b444d304568f75400732fa7397c643
杨丽娟 <1297578896@qq.com>
2020-04-05 22:32:57 +0800

    作业

---
+++ 1906101048-杨丽娟/day0331/作业-01.py
@@ -0,0 +1,19 @@
+class Stack(object):
+    def __init__(self):
+        self.stack = []
+    def sum_he(self,Date):
+        a=list(map(str,list(range(100))))
+        for date in Date:
+            if date in a:
+                self.stack.append(int(date))
+            if date == 'C':
+                self.stack = self.stack[:-1]
+            if date == 'D':
+                self.stack.append((self.stack[-1])*2)
+            if date == '+':
+                self.stack.append(sum(self.stack[-2:]))
+    def result(self):
+        return sum(self.stack)
+A=Stack()
+A.sum_he(["5","2","C","D","+"])
+print(A.result())
