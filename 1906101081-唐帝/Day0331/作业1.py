328309b1318bae175af6e9a730f0e330ac29402a
476072478 <476072478@qq.com>
2020-04-05 19:30:09 +0800

    0331

---
+++ 1906101081-唐帝/Day0331/作业1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def calPoints(self, ops):
+        last = []
+        for i in ops:
+            if i is "C":
+                last.pop()
+            elif i is "D":
+                last.append(last[-1] * 2)
+            elif i is "+":
+                last.append(last[-1] + last[-2])
+            else:
+                last.append(int(i))
+        return sum(last)
