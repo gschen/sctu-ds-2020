5a14f96a33c3a9b26526fbb94f696bae7aa48946
huangfuqin29 <2374104001@qq.com>
2020-04-07 16:37:27 +0800

    课堂练习

---
+++ 1906101030-黄富琴/day0407/test02.py
@@ -0,0 +1,34 @@
+class Queue:
+    #初始化队列
+    def __init__(self):
+        self.que=[]
+        self.size=0 #队列的长度
+
+    #判断队列是否为空
+    def is_empty(self):
+        if self.size==0:
+            return True
+        return False
+
+    #返回队列的长度
+    def que_size(self):
+        return self.size
+
+    #队列添加元素
+    def enqueue(self,value):
+        self.size+=1
+        self.que.append(value)
+
+    #删除队列元素
+    def dequeue(self):
+        if self.size==0:
+            print('没东西,不能删')
+            return 
+        else:
+            self.size-=1
+            self.que.pop(0)
+queue=Queue()
+queue.enqueue(1)
+queue.enqueue(2)
+print(queue.is_empty())
+print(queue.que_size())
