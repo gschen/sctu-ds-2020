e89902caa49ed024236e96d47b5f734fa77e3175
jiangwanglin <917563885@qq.com>
2020-04-28 16:46:49 +0800

    课堂笔记

---
+++ 1906101115-江汪霖/day0428/text02.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        if not root:
+            return True
+        def xxx(left,right):
+            if not left and not right:
+                return True
+            if not left or not right:
+                return False
+            if left.val != right.val:
+                return False
+
+            return xxx(left.left,right.right) and xxx(left.right,right.left)
+        return xxx(root.left,root.right)
