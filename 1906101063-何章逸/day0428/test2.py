b20c70202f1d367a5dd3cfd48dd3e63460634d23
he2307563520 <2307563520@qq.com>
2020-05-03 20:10:54 +0800

    笔记

---
+++ 1906101063-何章逸/day0428/test2.py
@@ -0,0 +1,22 @@
+#对称二叉树
+class soultion:
+def isSymmetric(self, root: TreeNode):
+    #如果为空
+    if not root:
+        return True
+
+    def dfs(root):
+        #如果做节点的值不等于右节点的值，返回False
+        if root.left.value!=root.right.value:
+            return False
+        #只有左节点或者右节点，而另一边没有，返回FALSE
+        if not root.left or root.right:
+            return False
+        #
+        if not root.left and not root.right
+            return True
+
+
+        return dfs(root.left) and dfs(root.right)
+
+    return dfs(root.left,root.right)
